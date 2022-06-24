361a9ad820fb28ee64f8df54a079052ed4db96eb
itskatherine <hurstkatherine@hotmail.co.uk>
2022-06-24 11:10:13 +0100

    Initialise React Native project

---
+++ l-earn/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+export default function App() {
+  {
+    /* <Image style={styles.pig} source={require("./assets/piggy.png")}></Image>
+      <Text>Spelling game!</Text> */
+  }
+  return (
+    <View style={{ backgroundColor: "#fff", flex: 1 }}>
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+      />
+      <View style={{ backgroundColor: "gold", flex: 1 }} />
+      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pig: {
+    width: 100,
+    height: 100,
+  },
+});
