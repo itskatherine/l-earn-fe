5dbb799f0be51536367bbf52e0ff0624d7db80d1
itskatherine <hurstkatherine@hotmail.co.uk>
2022-06-27 12:30:45 +0100

    Added AppHeader component to login screen

--- l-earn/app/screens/LoginScreen.js
+++ l-earn/app/screens/LoginScreen.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { ImageBackground, StyleSheet, Text } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import AppHeader from "../components/AppHeader/AppHeader";
 
 function LoginScreen(props) {
   return (
     <>
-      <AppHeader />
       <ImageBackground
         source={require("../assets/bg.jpg")}
         style={styles.background}
         imageStyle={{ opacity: 0.5 }}
-      ></ImageBackground>
+      >
+        <View style={styles.headerContainer}>
+          <AppHeader />
+        </View>
+      </ImageBackground>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  background: { flex: 1 },
+  background: { flex: 1, alignContent: "center" },
+  headerContainer: { alignItems: "center" },
 });
 
 export default LoginScreen;
