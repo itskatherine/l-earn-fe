3c71c4179a7f48cd4ca8532ea7cae25c5d6dc8b9
tobyw1234 <toby0977@gmail.com>
2022-07-06 16:54:45 +0100

    made pig change colour

--- l-earn/app/components/PiggyBank/PiggyBank.js
+++ l-earn/app/components/PiggyBank/PiggyBank.js
@@ -1,27 +1,80 @@
 import React from "react";
-import { Text, Image, View } from "react-native";
+import { Text, Image, View, Animated, Stylesheet, Easing } from "react-native";
 import formatMoney from "../../utils/formatMoney";
 import styles from "./styles";
 import { getUserFromId } from "../../utils/api";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ImageSwapper from "../../utils/imageswap";
 
-function PiggyBank({ amountEarned }) {
+
+const pigs = {pig:require("../../assets/piggybank.png"),goldpig:require("../../assets/piggybankgold.png")}
+
+function PiggyBank({ amountEarned, correct }) {
+
+  useEffect(() => {
+    if (correct === "correct") {
+      setCurrentImage(pigs.goldpig);
+    }
+    setTimeout(
+      () => {
+        setCurrentImage(pigs.pig);
+      },
+      10000,
+      
+    );
+     
+
+
+  }, [correct])
+
+
+
+  const [currentImage, setCurrentImage] = useState(
+    pigs.pig
+  );
+  const [pigscale, setPigscale] = useState(new Animated.Value(0));
+
+
   const [pocketMoney, setPocketMoney] = useState();
-  const getPocketMoney = getUserFromId(1).then((res) => {
+  const getPocketMoney = getUserFromId(1).then((res, pigpic) => {
     setPocketMoney(res.weekly_pocket_money);
   });
 
-  return (
-    <View style={styles.piggyBankIcon}>
-      <Image
-        style={styles.piggy}
-        source={require("../../assets/piggybank.png")}
-      ></Image>
-      <Text style={styles.text}>
-        {formatMoney(amountEarned)} / {formatMoney(pocketMoney)}
-      </Text>
-    </View>
-  );
-}
+  const pigScaleInterpolate = pigscale.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1],
+    extrapolate: "clamp",
+  });
+
+  const pigTransform = {
+    transform: [{ scale: pigScaleInterpolate }]
+  };
+
+  useEffect(() => {
+    Animated.timing(pigscale, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+      easing: Easing.linear,
+    }).start(() => {
+      setTimeout(() => {}, 500);
+    });
+  }, []);
+
+
+
+    return (
+      <Animated.View style={[styles.piggyBankIcon, pigTransform]}>
+        <Image
+          style={styles.piggy}
+          source={currentImage}
+        ></Image>
+        <Text style={styles.text}>
+          {formatMoney(amountEarned)} / {formatMoney(pocketMoney)}
+        </Text>
+      </Animated.View>
+    );
+  }
+
 
 export default PiggyBank;
